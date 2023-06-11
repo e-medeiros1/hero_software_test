b336fc14ec1b815f6e928dde2c71070e53b6d85b
Vinícius Schneider <mrschneider.v@gmail.com>
2023-06-10 21:48:01 -0300

    create fetch data from api function

---
+++ lib/app/core/functions/fetch_data_from_api.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' as http;
+
+Future<void> fetchApiData() async {
+  //URL da API deve ser referente a requisição que tu precisa.
+  //Passa ela como parâmetro e chama a função no passando a url do que tu precisa!
+  final url = Uri.parse('https://api.carro.cash/user/app/historic');
+  const token = 'b1933a3d-7d24-4c0c-baf1-663db105cce9';
+
+  final headers = {'security-token': token};
+
+  final response = await http.get(url, headers: headers);
+
+  if (response.statusCode == 200) {
+    final jsonData = response.body;
+    print(jsonData);
+  } else {
+    print('Falha na solicitação: ${response.statusCode}');
+  }
+}
